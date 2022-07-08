6a6f3caeacaf0dda2de16776f30ff77b33878ded
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 09:36:03 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//alert ("Mi archivo está bien linkeado")
+
+const primerSabor = prompt ('Puedes elegir tres sabores, ingresa el primero');
+const segundoSabor = prompt ('Ingresa tu segundo sabor deseado');
+const tercerSabor = prompt ('Ingresa tu tercer sabor deseado')
+
+alert ('Aquí tienes tu helado de ' +primerSabor +' '+ segundoSabor+'y' + tercerSabor );
+
+console.log (alert);
